b5f5e29e1ce4375c83438a8f26000019eb30bcfe
karisakib <36942613+karisakib@users.noreply.github.com>
2021-05-17 10:42:07 -0400

    folium and streamlit integrated successfully

--- about.py
+++ about.py
@@ -1,4 +1,17 @@
 import streamlit as st
 
+
 def app():
     st.title('About page')
+
+    st.header('Clariza Mayo')
+    st.text('About text')
+    
+    st.header('Stanislava Hristova')
+    st.text('About text')
+
+    st.header('Juan Peralta')
+    st.text('About text')
+
+    st.header('Kari Sakib')
+    st.text('About text')
