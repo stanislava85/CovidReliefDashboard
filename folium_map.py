d92e6369b3a96a78fde1a165d37654e118239350
Kai <kai@Kais-MBP.home>
2021-05-23 17:54:34 -0400

    made columns for covid map and sidebar within overview.py view

--- folium_map.py
+++ folium_map.py
@@ -4,9 +4,10 @@ import folium
 from folium.plugins import HeatMap
 import pandas as pd
 
+
 url = "https://raw.githubusercontent.com/nychealth/coronavirus-data/master/latest/last7days-by-modzcta.csv"
 df = pd.read_csv(url)
-icon_url = 'covid-red.png'
+icon_url = './img/covid-red.png'
 astoria_ditmars = [40.7644, -73.9235]
 popup = folium.Popup(min_width=5550)
 lat = df.lat.values
@@ -19,9 +20,11 @@ points = list(zip(lat, lon, zipcode, area, people_positive))
 m = folium.Map(location=astoria_ditmars, zoom_start=11, tiles="openstreetmap")
 for lat, lon, zipcode, area, people_positive in points:
     folium.Marker(
-        [lat,lon], popup=f"<p>Zipcode: {zipcode}{nl} Area: {area}{nl} Positive Cases: {people_positive}{nl}</p>",
-        icon=folium.features.CustomIcon(icon_url,icon_size=(10, 10))
+        [lat,
+            lon], popup=f"<p>Zipcode: {zipcode}{nl} Area: {area}{nl} Positive Cases: {people_positive}{nl}</p>",
+        icon=folium.features.CustomIcon(icon_url, icon_size=(15, 15))
     ).add_to(m)
+
 
 def app():
     # st.set_page_config(layout="wide")
@@ -29,3 +32,6 @@ def app():
     st.title('NYC Covid Map by Zipcode')
 
     folium_static(m)
+
+    st.write('This is a map of the five borough that make up New York City. We see many covid looking icons scattered all across the map. Each one of these icons represent an individual zipcode in New York City. If you zoom in and click on a covid icon, you will see a pop up that inform sus whhich zipcode it is, the name of the neighborhood, the current cases and the positivity rate within the past 7 days.')
+
