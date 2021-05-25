e442584f848c60aea24e7278c4bd34af258e146f
Kai <kai@Kais-MBP.home>
2021-05-24 20:21:41 -0400

    made filteres available for hypo one

--- hypothesis_one.py
+++ hypothesis_one.py
@@ -9,9 +9,15 @@ from hypothesis_two import clean_data
 group_data = clean_data()
 
 #Creating Filter variables
+bx, bk, mn, qn, si = "BX", "BK", "MN", "QN", "SI"
 boro_initials = "BK"
+boro_initials_list = ["BX", "BK", "MN", "QN", "SI"]
 borough_filter = f'{boro_initials}_CASE_COUNT'
 borough_fatality_filter = f'{boro_initials}_FATALITY_RATE'
+boro_names = ['Bronx', 'Brooklyn', 'Manhattan', 'Queens', 'Staten Island']
+
+def borough_filter_function(boro):
+    return f'{boro}_CASE_COUNT'
 
 #Declaring chart variables
 labels = ["0-17", "18-24", "25-34", "35-44", "45-54", "55-64", "65-74", "75+"]
@@ -23,7 +29,7 @@ y2 = group_data.iloc[:, 12:]
 data1 = group_data.groupby('subgroup')[borough_filter].sum()
 data2 = group_data.groupby('subgroup')[borough_fatality_filter].sum()
 
-def bar_1():
+def bar_1(boro):
 
     plt.style.use('fivethirtyeight')
     fig, ax1= plt.subplots(figsize=(30,15))
@@ -37,7 +43,7 @@ def bar_1():
     ax1.set_xlabel('Age', size=25)
     ax1.set_ylabel('Number of cases', size=25)
     ax2.set_ylabel('Fatality Rate', size=25)
-    ax1.set_title(f'Infection (Number of cases) and Fatality Rate(deaths/number of cases) Per Age Group in {borough_filter[:2]}', size=35)
+    ax1.set_title(f'Infection (Number of cases) and Fatality Rate(deaths/number of cases) Per Age Group in {borough_filter_function(boro)}', size=35)
     # plt.legend(loc='upper right', prop={"size":20})
     ax1.legend(loc='upper left', prop={"size":20})
     ax2.legend(loc='upper right',prop={"size":20})
@@ -63,16 +69,28 @@ def app():
     
     st.write(clean_data())
 
-    st.write('For this second diagram we are showing the correlation between the Number of\
-    	COVID-19 Cases and the Fatality Rate per Age Group. We have estimated the Fatality Rate\
-    	by dividing the Death Cases to the Number of Total Case Count per each Borough and Age Group.\
-		From this visualization we could conclude that there is no direct correlation between the\
-		number of cases per Age Group and the Fatality Rate per Age Group. This diagram proves our\
-		First Hypothesis where we have stated that the Fatality Rate increases as people\'s age\
-		increase. We see that the bar for Fatality Rate is the highest for the 75+ age group.')
+    option = st.selectbox('Select a Borough', (boro_names))
+    if option == boro_names[0]:
+        st.write(bar_1(bx))
+    elif option == boro_names[1]:
+        st.write(bar_1(bk))
+    elif option == boro_names[2]:
+        st.write(bar_1(mn))
+    elif option == boro_names[3]:
+        st.write(bar_1(qn))
+    else:
+        st.write(bar_1(si))
 
-    st.pyplot(bar_1())
+    # st.write('For this second diagram we are showing the correlation between the Number of\
+    # 	COVID-19 Cases and the Fatality Rate per Age Group. We have estimated the Fatality Rate\
+    # 	by dividing the Death Cases to the Number of Total Case Count per each Borough and Age Group.\
+	# 	From this visualization we could conclude that there is no direct correlation between the\
+	# 	number of cases per Age Group and the Fatality Rate per Age Group. This diagram proves our\
+	# 	First Hypothesis where we have stated that the Fatality Rate increases as people\'s age\
+	# 	increase. We see that the bar for Fatality Rate is the highest for the 75+ age group.')
 
-    st.write('This graph represents the distribution of COVID-19 Cases Per Age Group for Each Borough.\
-    	We need to select the Borough in order to see the Positive Cases distribution for each Age Group.')
-    st.pyplot(bar_2())
+    # st.pyplot(bar_1())
+
+    # st.write('This graph represents the distribution of COVID-19 Cases Per Age Group for Each Borough.\
+    # 	We need to select the Borough in order to see the Positive Cases distribution for each Age Group.')
+    # st.pyplot(bar_2())
