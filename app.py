bbdf586cf54b382dddbff616948a1c8d14c2ac02
Clariza Mayo <clarizamayo@MacBook-Pro.fios-router.home>
2021-06-19 18:07:46 -0400

    adding configuration

--- app.py
+++ app.py
@@ -3,13 +3,11 @@ import about
 import hypothesis_one
 import hypothesis_two
 import borough_cases
-# import folium_map
 import overview
 import streamlit as st
 
 PAGES = {
     "Home": home,
-    # "NYC Covid Map": folium_map,
     "Totals Overview": overview,
     "Borough Cases": borough_cases,
     "Hypothesis One": hypothesis_one,
@@ -18,7 +16,7 @@ PAGES = {
     "About": about,
 }
 
-# st.set_page_config(layout="wide")
+st.set_page_config(layout="wide")
 
 st.sidebar.title('Navigation')
 selection = st.sidebar.radio("Go to", list(PAGES.keys()))
