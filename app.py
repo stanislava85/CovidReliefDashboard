153ba4a00ef82a087dc9f3fd722e341a0fbc3f67
Clariza Mayo <clarizamayo@MacBook-Pro.fios-router.home>
2021-06-22 19:10:03 -0400

    adding presentation video

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import home
 import about
-import presentation
 import hypothesis_one
 import hypothesis_two
 import borough_cases
 import overview
+import presentation
 import streamlit as st
 
 PAGES = {
@@ -13,7 +13,7 @@ PAGES = {
     "Borough Cases": borough_cases,
     "Hypothesis One": hypothesis_one,
     "Hypothesis Two": hypothesis_two,
-    "Project Presentation": presentation
+    "Project Presentation": presentation,
     "About": about,
 }
 
--- presentation.py
+++ presentation.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 def app():
     st.title('TKH Data Science 2: Group Presentation')
-    st.video("img//Covid_Dashboard_Presentation")
+    st.video("img/Covid_Dashboard_Presentation.mp4")
